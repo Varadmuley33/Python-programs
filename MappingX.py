6dd73fef082e62e3dba2bf55b400c0f2af979739
Varad Muley <varad.muley408@gmail.com>
2026-05-17 21:19:15 +0530

    Add files via upload

---
+++ MappingX.py
@@ -0,0 +1,13 @@
+Information = {"Name":"Rahul","Age":25,"City":"Pune","Marks":89.90,"City":"Mumbai"}  #city vadavcli last city print hote
+
+print(Information)
+
+print(Information["City"])
+
+
+Information["Age"] = 26
+print(Information)
+
+
+
+
