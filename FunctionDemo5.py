3896789bb96fc555f54a3e4a78d6d4bc1ff0a92d
Varad Muley <varad.muley408@gmail.com>
2026-05-19 23:46:55 +0530

    Add files via upload

---
+++ FunctionDemo5.py
@@ -0,0 +1,13 @@
+#Accept : Accept one Parameter
+#Return : Nothing
+
+def Marvellous1(Value):
+    print("Inside Marvellous1 : ",Value)
+
+def main():
+    Marvellous1("Python")
+    Marvellous1(51)
+    
+
+if __name__ == "__main__":
+    main()  
