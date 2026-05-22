33b352e49663292291e3e36432f2663cfaa53c07
Varad Muley <varad.muley408@gmail.com>
2026-05-22 23:14:56 +0530

    Add files via upload

---
+++ SelectionEvenFunctionXX.py
@@ -0,0 +1,16 @@
+# Procedural
+
+def CheckEven(No):
+    if(No % 2 == 0):
+        print("It is even")
+    else:
+        print("It is odd")
+
+
+Value = 0
+
+print("Enter the number ")
+Value = int(input())
+
+CheckEven(Value)
+
