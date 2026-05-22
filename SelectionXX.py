ce37a8eb1a22ce6e2c2088d956d41716ebf89e32
Varad Muley <varad.muley408@gmail.com>
2026-05-22 23:14:06 +0530

    Add files via upload

---
+++ SelectionXX.py
@@ -0,0 +1,9 @@
+no = 21
+
+print(no + 2)   # 23
+print(no - 2)   # 19
+print(no * 2)   # 42
+print(no / 2)   # 10.5
+print(no ** 2)  # 441    (21 raised to 2  No raised to 2 in two * (square of no))
+print(no % 2)   # 1
+
