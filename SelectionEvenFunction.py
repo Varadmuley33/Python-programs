a0d3fd4f2e889f95c3a67c69abd093eb6b7c6427
Varad Muley <varad.muley408@gmail.com>
2026-05-22 23:14:41 +0530

    Add files via upload

---
+++ SelectionEvenFunction.py
@@ -0,0 +1,13 @@
+def CheckEven(No):
+    if(No % 2 == 0):
+        print("It is even")
+    else:
+        print("It is odd")
+
+def main():
+    CheckEven(21)                 #Positional
+    CheckEven(No = 22)            #Keyword argument
+
+
+if __name__ == "__main__":
+    main()
