71ca69128ab87a737bb24106b572a5e0440588bd
Varad Muley <varad.muley408@gmail.com>
2026-05-23 20:56:37 +0530

    Add files via upload

---
+++ Lambda2.py
@@ -0,0 +1,22 @@
+#Functional Approach
+
+
+CheckEven = lambda No : (No % 2 == 0)
+
+def main():
+    Value = 0
+    Ret = False
+
+    print("Enter the number ")
+    Value = int(input())
+
+    Ret = CheckEven(Value)
+
+    if(Ret == True):
+        print("It is even")
+    else:
+        print("It is Odd")
+            
+    
+if __name__ == "__main__":
+    main()
