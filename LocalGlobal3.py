29438ec3d36002e2c41e35286854330c2e97a8bf
Varad Muley <varad.muley408@gmail.com>
2026-05-25 23:39:18 +0530

    Add files via upload

---
+++ LocalGlobal3.py
@@ -0,0 +1,12 @@
+No = 11            #Global
+
+def Fun():
+    No = 21
+    print("Value of no from fun is : ",No)  #21
+    No = No + 1    
+    print("Value of no from fun is : ",No)  #22
+
+
+print("Value of No is : ",No)               #11
+Fun()
+print("Value of No is : ",No)              
