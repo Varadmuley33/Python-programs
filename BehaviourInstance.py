4139f612989e70e7189fb743f4a3acbe86961e22
Varad Muley <varad.muley408@gmail.com>
2026-05-30 23:37:26 +0530

    Add files via upload

---
+++ BehaviourInstance.py
@@ -0,0 +1,22 @@
+class Demo:
+    No = 10      #class variable
+
+    def __init__(self,A,B):    #It is written to create an instance
+        self.Value1 = A
+        self.Value2 = B
+
+    def fun(self):
+        print("Inside Instance method fun",self.Value1,self.Value2)
+
+    @classmethod
+    def sun(cls):
+        print("Inside Class method sun",cls.No)
+
+Demo.sun()
+print("Class Variable No : ",Demo.No)
+
+obj = Demo(11,21)
+
+obj.fun()
+print("Instance Variable : ",obj.Value1,obj.Value2)
+
