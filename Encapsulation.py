799292aad2ada61ea8dd889b316e27378193379c
Varad Muley <varad.muley408@gmail.com>
2026-05-31 21:16:01 +0530

    Add files via upload

---
+++ Encapsulation.py
@@ -0,0 +1,29 @@
+class Arithematic:
+    def __init__(self,A,B):
+        self.No1 = A
+        self.No2 = B
+        print("Object gets created successfully")
+
+    def Addition(self):
+        Ans = 0
+        Ans = self.No1 + self.No2
+        return Ans
+
+    def Substraction(self):
+        Ans = 0
+        Ans = self.No1 - self.No2
+        return Ans
+
+
+obj1 = Arithematic(11,10)        # Arithematic(id(obj1),11,10) -> __init__
+obj2 = Arithematic(21,20)
+
+
+Ret = 0
+
+Ret = obj1.Addition()
+print("Addition of numbers is : " ,Ret)
+
+
+Ret = obj2.Substraction()
+print("Substraction of numbers is : " ,Ret)
