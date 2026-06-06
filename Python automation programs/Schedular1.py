43057a237c112f6d427cdf3f4b67e548a8d7d821
Varad Muley <varad.muley408@gmail.com>
2026-06-06 23:29:52 +0530

    Add files via upload

---
+++ Python automation programs/Schedular1.py
@@ -0,0 +1,11 @@
+import time
+import datetime
+
+def main():
+    print(time.time())
+    print(time.ctime())
+    print(datetime.datetime.now())
+    
+if __name__ == "__main__":
+    main()
+#Linux epoc date yete yacha outpyr mahnje 1 jan 1970 pasun te aata parynt
