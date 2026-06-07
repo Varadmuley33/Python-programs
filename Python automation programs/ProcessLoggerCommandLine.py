3567d02a9cd34d7d5aaadebbed1d11d0b263a741
Varad Muley <varad.muley408@gmail.com>
2026-06-07 19:26:24 +0530

    Add files via upload

---
+++ Python automation programs/ProcessLoggerCommandLine.py
@@ -0,0 +1,55 @@
+#command line input
+
+import psutil
+import sys
+
+def main():
+    Border = "-"*50
+    print(Border)
+    print("------Marvellous Platform Surveillance System-----")
+    print(Border)
+
+    if(len(sys.argv) == 2):
+        if(sys.argv[1] == "--h" or sys.argv[1] == "--H"):
+            print("This script is used to : ")
+            print("1 : Create automatic logs")
+            print("2 : Executes periodically")
+            print("3 : Sends mail with the logs")
+            print("4 : Store information about process")
+            print("5 : Store information about CPU")
+            print("6 : Store information about RAM usage")
+            print("7 : Store information about secondary storage ")
+
+        elif(sys.argv[1] == "--u" or sys.argv[1] == "--U"):
+            print("Use the automation script as")
+            print("ScriptName.py TimeInterval DirectoryName")
+            print("TimeInterval : The time in minutes foe periodic scheduling ")
+            print("DirectoryName : NAme of Directory to create auto logs")
+
+        else:
+            print("Unable to proceed as there is no such option")
+            print("Please use --h or --u to get more details")
+
+    #python Demo.py 5 Marvellous
+    elif(len(sys.argv) == 3):
+        print("Inside Projects logic")
+        print("Time Interval : ",sys.argv[1])
+        print("Directory name : ",sys.argv[2])
+
+    else:
+        print("Inavlid number of command line arguments")
+        print("Unable to proceed as there is no such option")
+        print("Please use --h or --u to get more details")
+
+
+
+
+    print(Border)
+    print("----------Thank you for using our Script ---------")
+    print(Border)
+    
+    
+
+
+if __name__ == "__main__":
+    main()
